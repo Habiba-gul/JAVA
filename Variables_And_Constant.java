111f03386ad3f60d4aab21a868852e73582894bb
Habiba-gul <hgull9712@gmail.com>
2025-11-09 04:54:06 +0500

    java

---
+++ Variables_And_Constant.java
@@ -0,0 +1,16 @@
+public class Variables_And_Constant {
+    public static void main(String[] args) {
+        // Variable declaration and initialization
+        int age=20;
+        //we can also change the value of variable
+        age=21;
+        System.out.println("age is :"+age);
+        //we can also copy the value of one variable to another variable
+    
+    int myage=20;
+    int herage=myage;
+    System.out.println(myage);
+    System.out.println(herage);
+    
+}
+}
