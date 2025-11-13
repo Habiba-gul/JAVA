0cf90823e6d4b205227c986d6b1ea74a062c686d
Habiba-gul <hgull9712@gmail.com>
2025-11-13 07:45:27 +0500

    g

--- MultiDimensionalArray.java
+++ MultiDimensionalArray.java
@@ -1,12 +1,13 @@
 import java.util.Arrays;
 public class MultiDimensionalArray {
     public static void main(String[] args) {
+        //multi-dimension is array of arrays
         int[][] numbers=new int[3][2];
         numbers[0][0]=1;
          numbers[0][1]=2;
           numbers[1][0]=3;
            numbers[1][1]=1;
-           //it dives ref address becouse in two dimesion we dont use this state
+           //it gives ref address becouse in two dimesion we dont use this state
           // System.out.print(Arrays.toString(numbers));
 
           //we use deepto string for 2 dimensional array
@@ -15,6 +16,14 @@ public class MultiDimensionalArray {
           //compile time intialaization
           int[][] Arr={{1,2,3},{4,5,6},{5,3,8}};
           System.out.println(Arrays.deepToString(Arr));
+          //DISPLAY 2D ARRAY USINF FOR
+          int[][] scores={{1,2,3},{4,5,6},{6,7,8}};
+          for (int i = 0; i < scores.length; i++) {
+            for (int j = 0; j <scores[i].length; j++) {
+                System.out.print(scores[i][j]);
+            }
+            System.out.println();
+          }
 
     }
     
